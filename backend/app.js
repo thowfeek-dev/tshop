3e246a3e4759daa7d0689cc2726207f461248171
thowfeek salim <thowfeek.s@pillsbee.com>
2024-10-08 22:35:05 +0530

    added both cases

--- backend/app.js
+++ backend/app.js
@@ -5,26 +5,28 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(
-    cors({
-        origin: ["https://tshop-przo.vercel.app"],
-        credentials: true,
-    })
-);
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }));
+app.use(cors({
+  origin: 'https://tshop-przo.vercel.app',
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/test", (req, res) => {
-    res.send("Hello world!");
+  res.send("Hello world!");
 });
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({
-        path: "config/.env",
-    });
+  require("dotenv").config({
+    path: "config/.env",
+  });
 }
 
 // import routes
