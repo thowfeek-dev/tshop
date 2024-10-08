0059ce697409082a7d022e8a3c53243ab3fdc29d
thowfeek salim <thowfeek.s@pillsbee.com>
2024-10-08 19:34:52 +0530

    change row

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,3 @@
-export const server = "https://tshop-psi.vercel.app/api/v2";
-
-export const backend_url = "http://localhost:8000/";
+export const server = "https://eshop-tutorial.vercel.app/api/v2";
 
 
