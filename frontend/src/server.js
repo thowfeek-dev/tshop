a4e9812cc70fefdb5672ef7519cce40d94dd6b45
thowfeek salim <thowfeek.s@pillsbee.com>
2024-10-08 22:31:02 +0530

    added both cases

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,5 @@
+// export const server = "http://localhost:8000/api/v2";
+// export const backend_url = "http://localhost:8000/";
 export const server = "https://tshop-psi.vercel.app/api/v2";
+export const backend_url = "https://tshop-psi.vercel.app/";
 
-export const backend_url = "http://localhost:8000/";
